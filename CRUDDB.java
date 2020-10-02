4e421fbd7f74ced6613bc9ac4e15e69dfd6efbb2
dgiri12 <70178199+dgiri12@users.noreply.github.com>
2020-10-02 14:47:21 -0600

    SQL methods Spelling changed
    CRUDDB.java
    spelling changed for wholesale cost and supplier id

--- CRUDDB.java
+++ CRUDDB.java
@@ -13,7 +13,7 @@ public class CRUDDB
 
     public static String addProduct(Connection conn) throws SQLException {
         String print="";
-        String sql = "INSERT INTO cs3250main.sharktable (product_id, quantity, wholsale_cost, sale_price, suplier_id) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO cs3250main.sharktable (product_id, quantity, wholesale_cost, sale_price, supplier_id) VALUES (?, ?, ?, ?, ?)";
 
         PreparedStatement statement = conn.prepareStatement(sql);
 
@@ -40,9 +40,9 @@ public class CRUDDB
         while (result.next()){
             String pId = result.getString("product_id");
             String quan = result.getString("quantity");
-            String wCost = result.getString("wholsale_cost");
+            String wCost = result.getString("wholesale_cost");
             String sPrice = result.getString("sale_price");
-            String sId = result.getString("suplier_id");
+            String sId = result.getString("supplier_id");
 
             String output = "product: %s- %s- %s- %s- %s<br>";
             print+=String.format(output, count++, pId, quan, wCost, sPrice, sId);
@@ -53,7 +53,7 @@ public class CRUDDB
 
     public static String update(Connection conn) throws SQLException{
         String print="";
-        String updateSQL = "UPDATE cs3250main.sharktable SET quantity=?, wholsale_cost=?, sale_price=?, suplier_id=? WHERE product_id =? ";
+        String updateSQL = "UPDATE cs3250main.sharktable SET quantity=?, wholesale_cost=?, sale_price=?, supplier_id=? WHERE product_id =? ";
         PreparedStatement statement3 = conn.prepareStatement(updateSQL);
         statement3.setString(1,"5344");
         statement3.setString(2, "54.0");
