0409f2ffd07231175719a592eaf5d8b24814e9c2
dgiri12 <70178199+dgiri12@users.noreply.github.com>
2020-10-03 09:23:10 -0600

    GUI.java created
    Created GUI.java to store JFrames and objects, similar changes done to main as well. Static hasn't been taken care of yet

---
+++ GUI.java
@@ -0,0 +1,148 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+class Screen{
+    JPanel east = new JPanel();
+    JPanel west = new JPanel();
+    JPanel north = new JPanel();
+    JPanel south = new JPanel();
+    JPanel center = new JPanel();
+    JPanel bottom = new JPanel();
+
+    JFrame frame = new JFrame();
+
+    public Screen() { //does this constructor have to be public??
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //without this, frame is hidden but
+        frame.setPreferredSize(new Dimension(640, 480));
+    }
+
+    void packScreen(){//all objects have been added to their respective JPanels,
+        //now bind those JPanels into appropriate positions on this JFrame, and pack the JFrame
+        //this function should be called only after all objects have been added to their jpanels
+        frame.getContentPane().add(east, BorderLayout.EAST);
+        frame.getContentPane().add(west, BorderLayout.WEST);
+        frame.getContentPane().add(north, BorderLayout.NORTH);
+        frame.getContentPane().add(south, BorderLayout.SOUTH);
+        frame.getContentPane().add(center, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(false);
+    }
+
+}
+
+public class GUI {
+
+    ArrayList<Screen> screenList = new ArrayList<Screen>();
+    int screenIndexCounter=0;
+
+    ArrayList<JButton> buttonList = new ArrayList<JButton>();
+    int buttonIndexCounter=0;
+
+    ArrayList<JLabel> labelList = new ArrayList<JLabel>();
+    int labelIndexCounter=0;
+
+    //creates a screen, adds it to the storage, and returns an index to identify the screen in other
+    //functions to perform actions on it
+    int addScreen(){
+        Screen temp = new Screen();
+        screenList.add(temp);
+        screenIndexCounter++;
+        return screenIndexCounter-1;
+    }
+
+
+    //creates a button, adds it to the storage, and returns an index to identify the button in other
+    //functions to perform actions on it
+    int addButton(int _screenIndex, //which screen do you want the button to be in?
+                  String _buttonPlace, //which jpanel of the screen? center, right, left..?
+                  String _buttonName){ //name of the button
+
+        JButton temp = new JButton(_buttonName);
+
+        if (_buttonPlace=="center"){
+            screenList.get(_screenIndex).center.add(temp);
+        }
+        else if(_buttonPlace=="south"){
+            screenList.get(_screenIndex).south.add(temp);
+        }
+        else if(_buttonPlace=="east"){
+            screenList.get(_screenIndex).east.add(temp);
+        }
+        else if(_buttonPlace=="west"){
+            screenList.get(_screenIndex).west.add(temp);
+        }
+        else if(_buttonPlace=="north"){
+            screenList.get(_screenIndex).north.add(temp);
+        }
+        else if(_buttonPlace=="bottom") {
+            screenList.get(_screenIndex).bottom.add(temp);
+        }
+        else{
+            screenList.get(_screenIndex).center.add(temp);
+        }
+
+        buttonList.add(temp);
+        buttonIndexCounter++;
+        return buttonIndexCounter-1;
+
+    }
+    //creates a label, adds it to the storage, and returns an index to identify the label in other
+    //functions to perform actions on it
+    int addLabel(int _screenIndex, //which screen do you want the label to be in?
+                  String _labelPlace, //which jpanel of the screen? center, right, left..?
+                  String _labelName){ //name of the label
+
+        JLabel temp = new JLabel(_labelName);
+
+        if (_labelPlace=="center"){
+            screenList.get(_screenIndex).center.add(temp);
+        }
+        else if(_labelPlace=="south"){
+            screenList.get(_screenIndex).south.add(temp);
+        }
+        else if(_labelPlace=="east"){
+            screenList.get(_screenIndex).east.add(temp);
+        }
+        else if(_labelPlace=="west"){
+            screenList.get(_screenIndex).west.add(temp);
+        }
+        else if(_labelPlace=="north"){
+            screenList.get(_screenIndex).north.add(temp);
+        }
+        else if(_labelPlace=="bottom") {
+            screenList.get(_screenIndex).bottom.add(temp);
+        }
+        else{
+            screenList.get(_screenIndex).center.add(temp);
+        }
+
+        labelList.add(temp);
+        labelIndexCounter++;
+        return labelIndexCounter-1;
+    }
+
+    void setText(int _labelIndex, //the jlabel that you want to change
+                 String _text //the text you want to change it to
+                 ){
+        labelList.get(_labelIndex).setText(_text);
+    }
+
+    JButton getButton(int _buttonIndex){
+        return buttonList.get(_buttonIndex);
+    }
+
+    void setupGUI(){ //this function should only be called after creating all the GUI elements needed
+        for (int i=0;i<screenList.size();i++){
+            screenList.get(i).packScreen(); //pack each individual screen in the list
+        }
+    }
+
+    void screenChangeInto(int _screenIndex){ //only call this function after setupGUI has been called
+        for (int i=0;i<screenList.size();i++){ //make all the screen invisible
+            screenList.get(i).frame.setVisible(false);
+        }
+        screenList.get(_screenIndex).frame.setVisible(true); //make the specified screen visible
+    }
+
+}
--- Main.java
+++ Main.java
@@ -28,28 +28,32 @@ public class Main{
     static int crudRenderTest=0;
 
     //GUI component declarations
-    static JFrame mainFrame = new JFrame(); //the different frames this program is divided into
-    static JFrame simFrame = new JFrame();
-    static  JFrame crudFrame = new JFrame();
+    static GUI gui = new GUI();
+    static int mainScreen = gui.addScreen(); //the different screens this program is divided into
+    static int simScreen = gui.addScreen();
+    static int crudScreen = gui.addScreen();
+
     //buttons...
-    static JButton btnMainRunProgram = new JButton("Run Program");
-    static JButton btnMainRunSim = new JButton("Run Simulation");
-    static JButton btnSimGoBack = new JButton("Go back");
-    static JButton btnSimCustomer = new JButton("Invoke Customer Order");
-    static JButton btnSimSupplier = new JButton("Invoke Supplier Order");
-    static JButton btnSimDummy = new JButton("Dummy");
-    static JButton btnCrudGoBack = new JButton("Go back");
-    static JButton btnCrudCreate = new JButton("Create Product");
-    static JButton btnCrudRead = new JButton("Read Product");
-    static JButton btnCrudUpdate = new JButton("Update Product");
-    static JButton btnCrudDelete = new JButton("Delete Product");
+    static int btnMainRunProgram = gui.addButton(mainScreen,"center","Run Program");
+    static int btnMainRunSim = gui.addButton(mainScreen,"center","Run Simulation");
+    static int btnSimGoBack = gui.addButton(simScreen,"south","Go Back");
+    static int btnSimCustomer = gui.addButton(simScreen,"south","Invoke Customer Order");
+    static int btnSimSupplier = gui.addButton(simScreen,"south","Invoke Supplier Order");
+    static int btnSimDummy = gui.addButton(simScreen,"south","dummy");
+    static int btnCrudGoBack = gui.addButton(crudScreen,"south","Go Back");
+    static int btnCrudCreate = gui.addButton(crudScreen,"south","Create Product");
+    static int btnCrudRead = gui.addButton(crudScreen,"south","Read Product");
+    static int btnCrudUpdate = gui.addButton(crudScreen,"south","Update Product");
+    static int btnCrudDelete = gui.addButton(crudScreen,"south","Delete Product");
     //end  buttons...
+
     //labels...
-    static JLabel txtMainStatusBar = new JLabel(txtMainStatusBarOutput);
-    static JLabel txtSimTime = new JLabel("Time");
-    static JLabel txtSimOutput = new JLabel(simOutputStream.getStream());//this JLabel is output for the simulator
-    static JLabel txtSimOutput2 = new JLabel("");
-    static JLabel txtCrudOutput = new JLabel("CRUD output goes here");
+    static int txtSharkSoft = gui.addLabel(mainScreen,"north","StuffBuySharks Co. Warehouse Portal");
+    static int txtMainStatusBar = gui.addLabel(mainScreen,"south",txtMainStatusBarOutput);
+    static int txtSimTime = gui.addLabel(simScreen,"north","Time");
+    static int txtSimOutput = gui.addLabel(simScreen,"center",simOutputStream.getStream());
+    static int txtSimOutput2 = gui.addLabel(simScreen,"center","");
+    static int txtCrudOutput = gui.addLabel(crudScreen,"center","CRUD output goes here");
     //end labels...
 
     //database connection parameters
@@ -62,88 +66,19 @@ public class Main{
     static CRUDDB db1 = new CRUDDB(); //create the crud object
     static Connection conn; //connection object used to connect with DB
 
-    //METHODS FOR PROGRAM FUNCTIONS---------------------------------------------------------------
-
-    static void btnMainRunProgramMethod (){
-        mainFrame.setVisible(false);
-        crudFrame.setVisible(true);
-
-        //initialize crud process
-        crudRunning = true;
-    }
-    static void btnMainRunSimMethod (){
-        mainFrame.setVisible(false);
-        simFrame.setVisible(true);
-        simulationRunning=true; //activate simulation
-    }
-    static void btnSimGoBackMethod (){
-        simFrame.setVisible(false);
-        mainFrame.setVisible(true);
-        simulationRunning = false; //deactivate simulation
-    }
-    static void btnSimCustomerMethod (){
-        simOutputStream.push(simulator01.processBuyer(timeSeconds));
-    }
-    static void btnSimSupplierMethod (){
-        simOutputStream.push(simulator01.processSupplier(timeSeconds,dayCounter));
-    }
-    static void btnSimDummyMethod (){
-
-    }
-    static void btnCrudGoBackMethod (){
-        crudFrame.setVisible(false);
-        mainFrame.setVisible(true);
-        crudRunning=false;
-    }
-    static void btnCrudCreateMethod(){
-        String result="Waiting access...";
-        try{
-            result=db1.addProduct(conn); //function passes result onto the string
-        }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        crudOutputStream.push(result); //send result to stream for render later
-    }
-    static void btnCrudReadMethod(){
-        String result="Waiting access...";
-        try{
-            result=db1.select(conn); //function passes result onto the string
-        }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        crudOutputStream.push(result); //send result to stream for render later
-    }
-    static void btnCrudUpdateMethod(){
-        String result="Waiting access...";
-        try{
-            result=db1.update(conn); //function passes result onto the string
-        }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        crudOutputStream.push(result); //send result to stream for render later
-    }
-    static void btnCrudDeleteMethod (){
-        String result="Waiting access...";
-        try{
-            result=db1.delete(conn); //function passes result onto the string
-        }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        crudOutputStream.push(result); //send result to stream for render later
-    }
     static void appRender() {
 
-        txtMainStatusBar.setText(txtMainStatusBarOutput); //update the status bar of main frame
+        gui.setText(txtMainStatusBar,txtMainStatusBarOutput); //update the status bar of main frame
 
         if (crudRunning == true) {
             crudRenderTest++;
             if (crudRenderTest>10) crudRenderTest=0;
             crudOutputStream.push(Integer.toString(crudRenderTest));
-            txtCrudOutput.setText(crudOutputStream.getStream());
+            gui.setText(txtCrudOutput,crudOutputStream.getStream());
         }
         if (simulationRunning == true) {
             timeSeconds++; //add time
-            txtSimTime.setText("Time=" + Integer.toString(timeSeconds)); //print the time
+            gui.setText(txtSimTime,"Time=" + Integer.toString(timeSeconds)); //print the time
 
             //process the time
             if (timeSeconds >= 86400) {
@@ -158,8 +93,8 @@ public class Main{
             if (dayCounter > 6) simulationRunning = false;
 
             //finalize and render all result at the end of the frame
-            txtSimOutput.setText(simOutputStream.getStream());
-            txtSimOutput2.setText(simulator01.printTotalResult());
+            gui.setText(txtSimOutput,simOutputStream.getStream());
+            gui.setText(txtSimOutput2,simulator01.printTotalResult());
         }
     }
 
@@ -167,7 +102,9 @@ public class Main{
     //PROGRAM ENTRY POINT----------------------------
     public static void main(String[] args) {
 
-        GUIInit(); //initialize the GUI, buttons, actionListeners, labels, jpanels, etc.
+        gui.setupGUI();
+        gui.screenChangeInto(mainScreen); //start by making the first screen visible
+        setupListeners();
 
         simulator01.initializeSimulatorData(); //initialize the simulator
         timer.start(); //start rendering
@@ -187,178 +124,120 @@ public class Main{
             ex.printStackTrace();
         }
 
-    } //END PROGRAM-----------------------------------------------------------
 
+    }//END PROGRAM-----------------------------------------------------------
+
+    static void setupListeners(){
+        gui.getButton(btnMainRunProgram).addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            gui.screenChangeInto(crudScreen);
+            //initialize crud process
+            crudRunning = true;
+        }});
+
+    //SETUP RENDER TIMER FOR SIMULATION OUTPUT
+        timer.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) { //this timer handles our frames
+            appRender();
+        }
+    });
+    }//end setupListeners
     //METHOD FOR PROGRAM SETUP-----------------------------------------------
-    static void GUIInit(){
-        //MAINFRAME DEFINITION
 
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //without this, frame is hidden but
-        mainFrame.setPreferredSize(new Dimension(640, 480));
-
-        JLabel txtSharkSoft = new JLabel("SharkSoft"); //application title design
-
-        //create a panel and add the buttons to it
-        JPanel pnButton = new JPanel();
-        pnButton.add(btnMainRunProgram);
-        pnButton.add(btnMainRunSim);
-
-        //create bottom panel
-        JPanel pnMainBottom = new JPanel();
-        pnMainBottom.add(txtMainStatusBar);
-
-        //add objects to the frame
-        mainFrame.getContentPane().add(txtSharkSoft, BorderLayout.NORTH);
-        mainFrame.getContentPane().add(pnButton, BorderLayout.CENTER);
-        mainFrame.getContentPane().add(pnMainBottom,BorderLayout.SOUTH);
-        mainFrame.pack();
-        //MAINFRAME DEFINITION END----------------------
-
-        mainFrame.setVisible(true); //Just makes the frame visible
-
-        //SIMULATION SCREEN DEFINITION START--------------
-        simFrame.setPreferredSize(new Dimension(640, 480));
-        simFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //without this, frame is hidden but
-
-        //define the title panel
-        JPanel pnSimTitle = new JPanel();
-        JLabel txtSimTitle = new JLabel("Simulation");
-        pnSimTitle.add(txtSimTitle);
-        pnSimTitle.add(txtSimTime);
-
-        //define the screen panel
-        JPanel pnSimulationScreen = new JPanel();
-        Border blackline = BorderFactory.createLineBorder(Color.black);
-        txtSimOutput.setBorder(blackline);
-        txtSimOutput2.setBorder(blackline);
-        pnSimulationScreen.add(txtSimOutput); //rendering area 1
-        pnSimulationScreen.add(txtSimOutput2); //rendering area 2
-
-        //define and add to the buttons panel
-        JPanel pnSimBtn = new JPanel();
-        pnSimBtn.add(btnSimGoBack);
-        pnSimBtn.add(btnSimCustomer);
-        pnSimBtn.add(btnSimSupplier);
-        pnSimBtn.add(btnSimDummy);
-
-        simFrame.getContentPane().add(pnSimTitle, BorderLayout.NORTH);//title on top
-        simFrame.getContentPane().add(pnSimulationScreen, BorderLayout.CENTER); //renderin at center
-        simFrame.getContentPane().add(pnSimBtn, BorderLayout.SOUTH); //return button on the bottom
-
-        simFrame.pack(); //finalize properties
-
-        simFrame.setVisible(false);//keep invisible for now
-
-        //SIMULATION SCREEN DEFINITION END---------------------------
-
-        //CRUD SCREEN DEFINITION START--------------------------------
-
-        crudFrame.setPreferredSize(new Dimension(640, 480));
-        crudFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //without this, frame is hidden but
-
-        JPanel pnCrudTitle = new JPanel();
-        JLabel txtCrudTitle = new JLabel("CRUD");
-        pnCrudTitle.add(txtCrudTitle);
-
-        JPanel pnCrudCenter = new JPanel();
-
-        pnCrudCenter.add(txtCrudOutput);
-
-        JPanel pnCrudBottom = new JPanel();
-        pnCrudBottom.add(btnCrudGoBack);
-        pnCrudBottom.add(btnCrudCreate);
-        pnCrudBottom.add(btnCrudRead);
-        pnCrudBottom.add(btnCrudUpdate);
-        pnCrudBottom.add(btnCrudDelete);
-
-        crudFrame.getContentPane().add(pnCrudTitle, BorderLayout.NORTH);//title on top
-        crudFrame.getContentPane().add(pnCrudCenter, BorderLayout.CENTER); //rendering at center
-        crudFrame.getContentPane().add(pnCrudBottom, BorderLayout.SOUTH); //return button on the bottom
-
-        crudFrame.pack();
-
-        crudFrame.setVisible(false);
-
-        //CRUD SCREEN DEFINITION END---------------------
-
-        //ACTION LISTENERS, these codes binds the methods to the buttons
+        /*//ACTION LISTENERS, these codes binds the methods to the buttons
         //RUN SIMULATION BUTTON EVENT
-        btnMainRunProgram.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                btnMainRunProgramMethod();
-            }});
+
 
         btnMainRunSim.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                btnMainRunSimMethod();
+                gui.screenChangeInto(simScreen);
+                simulationRunning=true; //activate simulation
             }
         });
 
         //SIMULATION RETURN BUTTON EVENT
         btnSimGoBack.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-               btnSimGoBackMethod();
+                gui.screenChangeInto(mainScreen);
+                simulationRunning = false; //deactivate simulation
             }
         });
         btnSimCustomer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnSimCustomerMethod();
+                simOutputStream.push(simulator01.processBuyer(timeSeconds));
             }
         });
 
         btnSimSupplier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnSimSupplierMethod();
+                simOutputStream.push(simulator01.processSupplier(timeSeconds,dayCounter));
             }
         });
         btnSimDummy.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnSimDummyMethod();
+                //dummy
             }
         });
 
         btnCrudGoBack.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               btnCrudGoBackMethod();
+                gui.screenChangeInto(mainScreen);
+                crudRunning=false;
             }
         });
 
         btnCrudCreate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnCrudCreateMethod();
+                String result="Waiting access...";
+                try{
+                    result=db1.addProduct(conn); //function passes result onto the string
+                }catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                crudOutputStream.push(result); //send result to stream for render later
             }
         });
         btnCrudRead.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnCrudReadMethod();
+                String result="Waiting access...";
+                try{
+                    result=db1.select(conn); //function passes result onto the string
+                }catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                crudOutputStream.push(result); //send result to stream for render later
             }
         });
         btnCrudUpdate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnCrudUpdateMethod();
+                String result="Waiting access...";
+                try{
+                    result=db1.update(conn); //function passes result onto the string
+                }catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                crudOutputStream.push(result); //send result to stream for render later
             }
         });
         btnCrudDelete.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnCrudDeleteMethod();
+                String result="Waiting access...";
+                try{
+                    result=db1.delete(conn); //function passes result onto the string
+                }catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                crudOutputStream.push(result); //send result to stream for render later
             }
-        });
-
-        //SETUP RENDER TIMER FOR SIMULATION OUTPUT
-        timer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) { //this timer handles our frames
-                appRender();
-            }
-        });
+        });*/
     }
-}
