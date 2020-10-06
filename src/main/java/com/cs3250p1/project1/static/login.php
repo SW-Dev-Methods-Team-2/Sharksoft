1402f268b5190d5945fd0c36481c4ac06572d55e
Lfickling <lettiefickling@gmail.com>
2020-10-06 13:58:00 -0700

    readded static web pages to new wrapped application

---
+++ src/main/java/com/cs3250p1/project1/static/login.php
@@ -0,0 +1,38 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "localhost"; //I think that we have to add the host of the website later here     
+$dbservername =  "cs-3250-database-1testing.ctxpxr8jzoap.us-west-1.rds.amazonaws.com";
+$dbusername = "admin";
+$dbpassword = "cs3250db1";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (username, password)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
