ac1f008f30b48cc590b41f82800f1fb035a9d848
Lfickling <lettiefickling@gmail.com>
2020-10-10 13:32:34 -0700

    lookup classes (php & html) framework

--- src/static/lookup.php
+++ src/static/lookup.php
@@ -1,10 +1,7 @@
 <?php
 
-$email = filter_input(INPUT_POST, 'email');
-$user = filter_input(INPUT_POST, 'user_id');
-$productid = filter_input(INPUT_POST, 'itemid');
-$productquantity = filter_input(INPUT_POST, 'itemquant');
 
+$user = filter_input(INPUT_POST, 'user_id');
 
 $servername = "cs-3250-database-1testing.ctxpxr8jzoap.us-west-1.rds.amazonaws.com";
 $username = "admin";
@@ -14,15 +11,15 @@ $tablename = "sales_orders";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-  //echo "connected successfully";
+  echo "connected successfully";
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO $tablename (product_id, quantity, userID)
-VALUES ('$productid', '$productquantity', '$user')";
+//$sql = "INSERT INTO $tablename (product_id, quantity, userID)
+//VALUES ('$productid', '$productquantity', '$user')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
