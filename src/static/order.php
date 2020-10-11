45cb27ee79de7080cb8a0eaaa12b6a4d6b641a67
Lfickling <lettiefickling@gmail.com>
2020-10-10 19:47:49 -0700

    order form now displays back to user what they ordered

--- src/static/order.php
+++ src/static/order.php
@@ -1,34 +1,86 @@
-<?php
+<!DOCTYPE HTML>
+<html>
 
-$email = filter_input(INPUT_POST, 'email');
-$user = filter_input(INPUT_POST, 'user_id');
-$productid = filter_input(INPUT_POST, 'itemid');
-$productquantity = filter_input(INPUT_POST, 'itemquant');
+<head>
+    <meta charset="utf-8" />
+    <title>orderform</title>
+    <meta name="description" content="Order Processing">
+    <link rel="stylesheet" href="styles.css">
+</head>
 
 
-$servername = "cs-3250-database-1testing.ctxpxr8jzoap.us-west-1.rds.amazonaws.com";
-$username = "admin";
-$password = "cs3250db1";
-$dbname = "cs3250main";
-$tablename = "sales_orders";
+<body class="">
+    <div class="container">
+        <div class="nav-wrapper">
+            <div class="leftside">
+                <div class="brand">
+                    <div>STUFFBUYSHARKS</div>
+                </div>
+                <div class="motto">
+                    <div>THE ONE STOP SHOP FOR ALL YOUR SHARK NEEDS</div>
+                </div>
+            </div>
+            <div class="rightside">
+                <div class="nav-linkwrapper">
+                    <a href="index.html">Home</a>
+                </div>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-  //echo "connected successfully";
+                <div class="nav-linkwrapper">
+                    <a href="orderform.html">Order Form</a>
+                </div>
+                <!-- 
+                <div class="nav-linkwrapper">
+                    <a href="login.html">Login</a>
+                </div>
+                -->
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+            </div>
+        </div>
+    </div>
+    <div class="content-wrapper">
+       <div class="order-form-wrapper">
+            <h1>Thank you for your order!</h1>
+       </div>
+    </div>
+ 
+  <?php
 
-$sql = "INSERT INTO $tablename (product_id, quantity, userID)
-VALUES ('$productid', '$productquantity', '$user')";
+  $servername = "cs-3250-database-1testing.ctxpxr8jzoap.us-west-1.rds.amazonaws.com";
+  $username = "admin";
+  $password = "cs3250db1";
+  $dbname = "cs3250main";
+  $tablename = "sales_orders";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+    //echo "connected successfully";
 
-$conn->close();
-?>
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $email = filter_input(INPUT_POST, 'email');
+  $useraddress = filter_input(INPUT_POST, 'address');
+  $user = filter_input(INPUT_POST, 'user_id');
+   
+  $productid = filter_input(INPUT_POST, 'itemid');
+  $productquantity = filter_input(INPUT_POST, 'itemquant');
+
+  $sql = "INSERT INTO $tablename (product_id, quantity, userID)
+  VALUES ('$productid', '$productquantity', '$user')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "<b>Your order: </b><br><br>";
+    echo "<b>User ID:</b> $user <br><br><b>Email:</b> $email <br><br><b>Shipping Address:</b> $useraddress<br><br>";
+    echo "<b>Item ID#:</b> $productid  <b>Quantity:</b> $productquantity";
+  } else {
+    echo "<br>There was an error with your order: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+  ?>
+
+</body> 
+
+</html>
