82289c9241714c3c6b8f233bb27abb86ea3f8b6d
jgonzo3224 <jgonz157@msudenver.edu>
2020-11-08 20:52:34 -0700

    added a work in progress update/delete file

---
+++ src/Website/Resources/Updateorder.php
@@ -0,0 +1,33 @@
+<?php
+
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+if ($link->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Delete order method
+$deleteOrder = "DELETE FROM sales_orders WHERE id=3";
+
+if ($link->query($deleteOrder) === TRUE) {
+    echo "Order Cancelled Successfully";
+} else {
+    echo "Error Order Not Cancelled: " .$link->error;
+}
+
+//Update Order Method
+$editOrder = "UPDATE sales_orders SET WHERE id=3";
+
+if ($link->query($editOrder) === TRUE) {
+    echo "Order Updated Successfully";
+} else {
+    echo "Error Order Not Updated: " .$link->error;
+}
+?>
